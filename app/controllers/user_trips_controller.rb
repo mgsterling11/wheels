01bb6ffcc1d84515219279a93a1518fc63590b5b
AmandaKJohns <amandajohns77@gmail.com>
2015-11-03 21:15:34 -0500

    refactor build_url and user_trips_controller create method

--- app/controllers/user_trips_controller.rb
+++ app/controllers/user_trips_controller.rb
@@ -1,23 +1,23 @@
 class UserTripsController < ApplicationController
 
-
   def new
     render 'welcome'
   end
 
   def create
-    @origin = Origin.find_or_create_by(address: params[:address1])
-    @destination = Destination.find_or_create_by(address: params[:address2])
-    ride = TaxiData.new
-    search_results = ride.build_url(@origin.longitude, @origin.latitude, @destination.longitude, @destination.latitude)
-    @cost = ride.calculate_fare(search_results)
-    @time = ride.calculate_time(search_results)
+    @trip = UserTrip.new
+    @trip.build_origin(address: params[:address1])
+    @trip.build_destination(address: params[:address2])
+    @trip.save
+  
+    data = TaxiData.new
+    url = data.build_url(@trip)
+    search_results = data.connection(url)
+   
+    @cost = data.calculate_fare(search_results)
+    @time = data.calculate_time(search_results)
     render 'show'
-    # @search_results = Search.for(params[:address1, :address2])
-    # @location = Location.new(params)
-    # @latitude = @location.latitude
-    # binding.pry
-    # @longitude = @location.longitude
+    
   end
 
 # New search => 
--- app/models/taxi_data.rb
+++ app/models/taxi_data.rb
@@ -7,25 +7,47 @@ require 'pry'
 
 class TaxiData
 
-  def build_url(plong, plat, dlong, dlat)
-    pickup_longitude = plong
-      plong1 = pickup_longitude - 0.001
-      plong2 = pickup_longitude + 0.001
-    pickup_latitude = plat
-      plat1 = pickup_latitude - 0.001
-      plat2 = pickup_latitude + 0.001
-    dropoff_longitude = dlong
-      dlong1 = dropoff_longitude - 0.001
-      dlong2 = dropoff_longitude + 0.001
-    dropoff_latitude = dlat
-      dlat1 = dropoff_latitude - 0.001
-      dlat2 = dropoff_latitude + 0.001
-
-    url_string = "https://data.cityofnewyork.us/resource/2yzn-sicd.json?$limit=50000&$where=pickup_longitude%20%3E%20#{plong1}%20AND%20pickup_longitude%20%3C%20#{plong2}%20AND%20pickup_latitude%20%3E%20#{plat1}%20AND%20pickup_latitude%20%3C%20#{plat2}%20AND%20dropoff_longitude%20%3E%20#{dlong1}%20AND%20dropoff_longitude%20%3C%20#{dlong2}%20AND%20dropoff_latitude%20%3E%20#{dlat1}%20AND%20dropoff_latitude%20%3C%20#{dlat2}"
+  # MOVE TO ADAPTER??
+  def connection(url_string)
     data = RestClient::Request.execute(method: :get, url: url_string)
     test_data = JSON.load(data)
   end
 
+  def build_url(trip)
+    params = {pickup_longitude: trip.origin.longitude, pickup_latitude: trip.origin.latitude, dropoff_longitude: trip.destination.longitude, dropoff_latitude: trip.destination.latitude}
+
+    url_string = "https://data.cityofnewyork.us/resource/2yzn-sicd.json?$limit=50000&$where="
+
+      params.each do | attribute, data |
+        range1 = data - 0.001
+        range2 = data + 0.001
+        url_string += "#{attribute}%20%3E%20#{range1}%20AND%20#{attribute}%20%3C%20#{range2}%20AND%20"
+      end
+
+    url_string.chomp("%20AND%20")
+
+  end  
+
+# BUILD GREEN URL
+  def build_green_url(trip)
+    params = {pickup_longitude: trip.origin.longitude, pickup_latitude: trip.origin.latitude, dropoff_longitude: trip.destination.longitude, dropoff_latitude: trip.destination.latitude}
+
+    url_string = "https://data.cityofnewyork.us/resource/utt9-dvgj.json?$limit=50000&$where="
+
+      params.each do | attribute, data |
+        range1 = data - 0.001
+        range2 = data + 0.001
+        url_string += "#{attribute}%20%3E%20#{range1}%20AND%20#{attribute}%20%3C%20#{range2}%20AND%20"
+      end
+
+    url_string.chomp("%20AND%20")
+
+  end  
+
+  # -------END ADAPTER
+
+
+  # KEEP IN TAXI DATA CLASS
   def calculate_fare(results)
    
     num = results.map do |hash|
@@ -46,5 +68,24 @@ class TaxiData
   end
 end
 
+# OLD STRING CONSTRUCTION
+# ----------------------------------
+# def build_url(plong, plat, dlong, dlat)
+  #   pickup_longitude = plong
+  #     plong1 = pickup_longitude - 0.001
+  #     plong2 = pickup_longitude + 0.001
+  #   pickup_latitude = plat
+  #     plat1 = pickup_latitude - 0.001
+  #     plat2 = pickup_latitude + 0.001
+  #   dropoff_longitude = dlong
+  #     dlong1 = dropoff_longitude - 0.001
+  #     dlong2 = dropoff_longitude + 0.001
+  #   dropoff_latitude = dlat
+  #     dlat1 = dropoff_latitude - 0.001
+  #     dlat2 = dropoff_latitude + 0.001
 
+  #   url_string = "https://data.cityofnewyork.us/resource/2yzn-sicd.json?$limit=50000&$where=pickup_longitude%20%3E%20#{plong1}%20AND%20pickup_longitude%20%3C%20#{plong2}%20AND%20pickup_latitude%20%3E%20#{plat1}%20AND%20pickup_latitude%20%3C%20#{plat2}%20AND%20dropoff_longitude%20%3E%20#{dlong1}%20AND%20dropoff_longitude%20%3C%20#{dlong2}%20AND%20dropoff_latitude%20%3E%20#{dlat1}%20AND%20dropoff_latitude%20%3C%20#{dlat2}"
+  #   data = RestClient::Request.execute(method: :get, url: url_string)
+  #   test_data = JSON.load(data)
+  # end
 
