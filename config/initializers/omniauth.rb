857c82e4ac82ca3af6960ee644ae6c5439584204
AmandaKJohns <amandajohns77@gmail.com>
2015-11-06 10:02:23 -0500

    fix uber omniauth signin and add uber button to show page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
-  provider :uber, ENV['UBER_APP_ID'], ENV['UBER_APP_SECRET']
+  provider :uber, ENV['UBER_APP_ID'], ENV['UBER_APP_SECRET'], :scope => 'profile'
 end
