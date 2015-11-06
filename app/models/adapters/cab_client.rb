9d222843963d368c07e74653ab70480ebcfe0993
jrectenwald <jimrec101@gmail.com>
2015-11-06 09:08:48 -0500

    destroy my_trips button

--- app/models/adapters/cab_client.rb
+++ app/models/adapters/cab_client.rb
@@ -19,15 +19,12 @@ module Adapters
       params = {pickup_longitude: trip.origin.longitude, pickup_latitude: trip.origin.latitude, dropoff_longitude: trip.destination.longitude, dropoff_latitude: trip.destination.latitude}
 
       url_string = "https://data.cityofnewyork.us/resource/2yzn-sicd.json?$limit=50000&$where="
-      binding.pry
         params.each do | attribute, data |
           range1 = data - 0.001
           range2 = data + 0.001
           url_string += "#{attribute}%20%3E%20#{range1}%20AND%20#{attribute}%20%3C%20#{range2}%20AND%20"
         end
-
       url_string.chomp("%20AND%20")
-
     end 
 
      def build_green_cab_url(trip)
